52017769d37ec4fde2f533d6f06f302ecf2dc4e8
Saleel <saleel@me.com>
2023-03-10 17:41:14 +0530

    feat: show group name in feedback list page

--- apps/web-app/src/pages/events/[groupId]/index.tsx
+++ apps/web-app/src/pages/events/[groupId]/index.tsx
@@ -1,34 +1,60 @@
-
 import React from "react"
-import { Spinner, Text } from "@chakra-ui/react"
+import { Box, Button, Card, CardBody, Heading, Spinner, Text } from "@chakra-ui/react"
 import { useRouter } from "next/router"
 import Subgraph from "../../../subgraph"
 import usePromise from "../../../hooks/use-promise"
+import API from "../../../API"
 
 export default function NewFeedbackPage() {
     const router = useRouter()
     const { groupId } = router.query
 
-    const [feedback, { isFetching, error }] = usePromise<any[]>(() => Subgraph.getFeedbacksForEvent(groupId as string), {
-        conditions: [groupId],
-        dependencies: [groupId],
-        defaultValue: []
-    })
+    const [group, { isFetching: isFetchingGroup, error: apiError }] = usePromise(
+        () => API.getGroup(groupId as string),
+        {
+            conditions: [groupId],
+            defaultValue: {}
+        }
+    )
 
-    if (isFetching) {
+    const [feedback, { isFetching: isFetchingFeedback, error: subgraphError }] = usePromise<any[]>(
+        () => Subgraph.getFeedbacksForEvent(groupId as string),
+        {
+            conditions: [groupId],
+            dependencies: [groupId],
+            defaultValue: []
+        }
+    )
+
+    if (isFetchingGroup || isFetchingFeedback) {
         return <Spinner />
     }
 
-    if (error) {
+    if (subgraphError || apiError) {
         return <Text>Error while fetching feedback for group</Text>
     }
 
     return (
         <>
-            {feedback.map(f => (
-              <Text>{f.feedback}</Text>
-            ))}
+            <Heading size="md" mb="2rem">
+                {group.name}
+            </Heading>
+
+            <Box mb="5rem">
+                {feedback.map((f) => (
+                    <Card>
+                        <CardBody>
+                            <Text>{f.feedback}</Text>
+                        </CardBody>
+                    </Card>
+                ))}
+            </Box>
+
+            <Text fontSize={12} mb={1}>Do you have more to say? You can leave feedback up to 5 times.</Text>
+
+            <Button colorScheme="gray" onClick={() => router.push(`/events/${groupId}/new`)}>
+                Share Again
+            </Button>
         </>
     )
 }
-
